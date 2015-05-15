c38310a57dc97cdfc7c04bddb70586087b99a431
AmandaKedaigle <mandyjoy@mit.edu>
2015-05-15 10:35:51 -0400

    Typos in testing script

--- example/a549/test-tgfb-data.py
+++ example/a549/test-tgfb-data.py
@@ -13,10 +13,12 @@ if __name__=='__main__':
     parser=OptionParser()
     parser.add_option('--forest-only',dest='forest_only',action='store_true',default=False,help='Set this flag to run forest on phospho-proteomic data only.  DEFAULT:%default')
     parser.add_option('--msgsteiner',dest='msgsteiner',type='string',help='Path to msgsteiner9 code, be sure to include!')
-    parser.add_option('--doRandom',dest='rand',action='store_true',help='THIS WILL TAKE A LONG TIME: set this flag to do 100 permutations of forest using the --noisyEdges flag',default=False)
+    parser.add_option('--doRandom',dest='rand',action='store_true',help='THIS WILL TAKE A LONG TIME: set this flag to do 50 permutations of forest using the --noisyEdges flag',default=False)
     
     opts,args=parser.parse_args()
     
+    phos_weights='Tgfb_phos.txt'
+
     #garnet requires a configuration file that has all the data
     forest_out='tgfb_garnet_forest_output'
     garnet_conf='tgfb_garnet.cfg' #provided config file
@@ -51,7 +53,7 @@ if __name__=='__main__':
             os.system('mkdir '+forest_out)
         fcmd='python ../../scripts/forest.py --prize=%s --edge=%s --conf=%s  --outpath=%s --msgpath=%s --dummyMode=%s'%(phos_weights,edge_file,forest_conf,forest_out,msgsteinerpath,dummy_nodes)
         if opts.rand:
-            fcmd=fcmd+' --noisyEdges=20'
+            fcmd=fcmd+' --noisyEdges=50'
         print '\n'+fcmd
 
         os.system(fcmd)
