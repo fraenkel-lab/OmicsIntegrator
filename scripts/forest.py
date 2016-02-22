877b4e7e1bb9bfd850b901a9b611532ce4d8429e
AmandaKedaigle <mandyjoy@mit.edu>
2016-02-22 17:10:46 -0500

    Changed optparse to argparse

--- scripts/forest.py
+++ scripts/forest.py
@@ -4,7 +4,7 @@
 #2013-2014
 
 
-import sys, optparse, random, copy, os
+import sys, argparse, random, copy, os
 import subprocess, tempfile
 import networkx as nx
 from operator import itemgetter
@@ -1246,74 +1246,74 @@ def crossValidation(k, rep, PCSFInputObj, seed, msgpath, outputpath, outputlabel
 
 def main():
     #Parsing arguments (run python PCSF.py -h to see all these decriptions)
-    parser = optparse.OptionParser(description='Find multiple pathways within an interactome '\
+    parser = argparse.ArgumentParser(description='Find multiple pathways within an interactome '\
         'that are altered in a particular condition using the Prize Collecting Steiner Forest '\
         'problem')
     #required arguments
-    parser.add_option("-p", "--prize", dest='prizeFile', help='(Required) Path to the text file '\
+    parser.add_argument("-p", "--prize", dest='prizeFile', help='(Required) Path to the text file '\
         'containing the prizes. Should be a tab delimited file with lines: "ProteinName'\
         '\tPrizeValue"')
-    parser.add_option("-e", "--edge", dest='edgeFile', help ='(Required) Path to the text file '\
+    parser.add_argument("-e", "--edge", dest='edgeFile', help ='(Required) Path to the text file '\
         'containing the interactome edges. Should be a tab delimited file with 3 or 4 columns: '\
         '"ProteinA\tProteinB\tWeight(between 0 and 1)\tDirectionality(U or D, optional)"')
     #optional arguments
-    parser.add_option("-c", "--conf", dest='confFile', help='Path to the text file containing '\
+    parser.add_argument("-c", "--conf", dest='confFile', help='Path to the text file containing '\
         'the parameters. Should be several lines that looks like: "ParameterName = '\
         'ParameterValue". Must contain values for w, b, D.  May contain values for optional '\
         'parameters mu, n, r, g. Default = "./conf.txt"', default='conf.txt')
-    parser.add_option("-d","--dummyMode", dest='dummyMode', help='Tells the program which nodes '\
+    parser.add_argument("-d","--dummyMode", dest='dummyMode', help='Tells the program which nodes '\
         'in the interactome to connect the dummy node to. "terminals"= connect to all terminals, '\
         '"others"= connect to all nodes except for terminals, "all"= connect to all '\
         'nodes in the interactome. If you wish you supply your own list of proteins, dummyMode '\
         'could also be the path to a text file containing a list of proteins (one per line). '\
-        'Default = "terminals"', default='terminals')
-    parser.add_option("--garnet", dest='garnet', help='Path to the text file containing '\
+	'Default = "terminals"', default='terminals')
+    parser.add_argument("--garnet", dest='garnet', help='Path to the text file containing '\
         'the output of the GARNET module regression. Should be a tab delimited file with 2 '\
         'columns: "TranscriptionFactorName\tScore". Default = "None"', default=None)
-    parser.add_option("--musquared", action='store_true', dest='musquared', help='Flag to add '\
+    parser.add_argument("--musquared", action='store_true', dest='musquared', help='Flag to add '\
         'negative prizes to hub nodes proportional to their degree^2, rather than degree. Must '\
         'specify a positive mu in conf file.', default=False)
-    parser.add_option("--excludeTerms", action='store_true', dest='excludeT', help='Flag to '\
+    parser.add_argument("--excludeTerms", action='store_true', dest='excludeT', help='Flag to '\
         'exclude terminals when calculating negative prizes. Use if you want terminals to keep '\
         'exact assigned prize regardless of degree.', default=False)
-    parser.add_option("--msgpath", dest='msgpath',  help='Full path to the message passing code. '\
+    parser.add_argument("--msgpath", dest='msgpath',  help='Full path to the message passing code. '\
         'Default = "<current directory>/msgsteiner9"', default='./msgsteiner9')
-    parser.add_option("--outpath", dest = 'outputpath', help='Path to the directory which will '\
+    parser.add_argument("--outpath", dest = 'outputpath', help='Path to the directory which will '\
         'hold the output files. Default = this directory', default='.')
-    parser.add_option("--outlabel", dest = 'outputlabel', help='A string to put at the beginning '\
+    parser.add_argument("--outlabel", dest = 'outputlabel', help='A string to put at the beginning '\
         'of the names of files output by the program. Default = "result"', default='result')
-    parser.add_option("--cyto30", action='store_true', dest='cyto30', help='Use this flag if '\
+    parser.add_argument("--cyto30", action='store_true', dest='cyto30', help='Use this flag if '\
         'you want the output files to be amenable with Cytoscape v3.0 (this is the default).',
         default=True)
-    parser.add_option("--cyto28", action='store_false', dest='cyto30', help='Use this flag if '\
+    parser.add_argument("--cyto28", action='store_false', dest='cyto30', help='Use this flag if '\
         'you want the output files to be amenable with Cytoscape v2.8, rather than v3.0.')
-    parser.add_option("--noisyEdges", dest='noiseNum', help='An integer specifying how many '\
+    parser.add_argument("--noisyEdges", dest='noiseNum', help='An integer specifying how many '\
         'times you would like to add noise to the given edge values and re-run the algorithm. '\
         'Results of these runs will be merged together and written in files with the word '\
-        '"_noisyEdges_" added to their names. Default = 0', type='int', default=0)
-    parser.add_option("--shuffledPrizes", dest='shuffleNum', help='An integer specifying how '\
+        '"_noisyEdges_" added to their names. Default = 0', type=int, default=0)
+    parser.add_argument("--shuffledPrizes", dest='shuffleNum', help='An integer specifying how '\
         'many times you would like to shuffle around the given prizes and re-run the algorithm. '\
         'Results of these runs will be merged together and written in files with the word '\
-        '"_shuffledPrizes_" added to their names. Default = 0', type='int', default=0)
-    parser.add_option("--randomTerminals", dest='termNum', help='An integer specifying how many '\
+        '"_shuffledPrizes_" added to their names. Default = 0', type=int, default=0)
+    parser.add_argument("--randomTerminals", dest='termNum', help='An integer specifying how many '\
         'times you would like to apply your given prizes to random nodes in the interactome (with'\
         ' a similar degree distribution) and re-run the algorithm. Results of these runs will be '\
         'merged together and written in files with the word "_randomTerminals_" added to their '\
-        'names. Default = 0', type='int', default=0)
-    parser.add_option("--knockout", dest='knockout', help='A list specifying protein(s) you '\
-        'would like to "knock out" of the interactome to simulate a knockout experiment, '\
-        "i.e. ['TP53'] or ['TP53', 'EGFR'].", type='string', default='[]')
-    parser.add_option("-k", "--cv", dest='cv', help='An integer specifying the k value if you '\
+        'names. Default = 0', type=int, default=0)
+    parser.add_argument("--knockout", dest='knockout', nargs='*', help='Protein(s) you '\
+        'would like to "knock out" of the interactome to simulate a knockout experiment.', \
+	default=[])    
+    parser.add_argument("-k", "--cv", dest='cv', help='An integer specifying the k value if you '\
         'would like to run k-fold cross validation on the prize proteins. Default = None.', \
-        type='int', default=None)
-    parser.add_option("--cv-reps", dest='cv_reps', help='An integer specifying how many runs of '\
+        type=int, default=None)
+    parser.add_argument("--cv-reps", dest='cv_reps', help='An integer specifying how many runs of '\
         'cross-validation you would like to run. To use this option, you must also specify a -k '\
-        'or --cv parameter. Default = None.', type='int', default=None)
-    parser.add_option("-s", "--seed", dest='seed', help='An integer seed for the pseudo-random '\
+        'or --cv parameter. Default = None.', type=int, default=None)
+    parser.add_argument("-s", "--seed", dest='seed', help='An integer seed for the pseudo-random '\
         'number generators. If you want to reproduce exact results, supply the same seed. '\
-        'Default = None.', type='int', default=None)
+        'Default = None.', type=int, default=None)
     
-    (options, args) = parser.parse_args()
+    options = parser.parse_args()
     
     #Check cv parameters do not conflict
     if options.cv_reps != None:
