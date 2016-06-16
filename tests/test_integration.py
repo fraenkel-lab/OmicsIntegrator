10fab3c0d2f400670fa182bdfb96ba9f3fb30dc3
Anthony Gitter <gitter@biostat.wisc.edu>
2016-06-16 09:00:16 -0500

    Simplify integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -4,27 +4,17 @@ Large integration test script.
 '''
 
 import os, subprocess, filecmp, shutil, shlex, tempfile
-from optparse import OptionParser
 
+# msgsteiner is a fixture object
 def test_integration(msgsteiner):
-
-    parser = OptionParser()
-
-    parser.add_option('--msgpath',dest='msgsteiner',type='string',help='Path to msgsteiner code, be sure to include!')
-
+    # msgsteiner is parsed using conftest.py
+    assert msgsteiner is not None, 'Please provide path to msgsteiner using --msgpath option'
+    
+    # Forest inputs
+    forest_conf = os.path.join(os.path.dirname(__file__), '..', 'example', 'a549', 'tgfb_forest.cfg') #provided config file
     phos_weights = os.path.join(os.path.dirname(__file__), '..', 'example', 'a549', 'Tgfb_phos.txt')
 
-    #provided config file
-    garnet_conf = os.path.join(os.path.dirname(__file__), '..', 'example', 'a549', 'tgfb_garnet.cfg')
-
-    #forest requires more inputs
-    forest_conf = os.path.join(os.path.dirname(__file__), '..', 'example', 'a549', 'tgfb_forest.cfg') #provided config file
-
     edge_file = os.path.join(os.path.dirname(__file__), '..', 'data', 'iref_mitab_miscore_2013_08_12_interactome.txt') #interactom
-
-    #WE NEED MSGSTEINER INSTALLED!!!
-    msgsteinerpath = msgsteiner
-    assert msgsteinerpath is not None, 'Please provide path to msgsteiner using --msgpath option'
 
     # Create a tmp directory for output	
     forest_out = tempfile.mkdtemp()
@@ -38,7 +28,7 @@ def test_integration(msgsteiner):
         forest_path = os.path.join(script_dir, '..', 'scripts', 'forest.py')
     
         fcmd='python %s --prize=%s --edge=%s --conf=%s  --outpath=%s --msgpath=%s --seed=%s'%(
-            forest_path, phos_weights, edge_file, forest_conf, forest_out, msgsteinerpath, seed)
+            forest_path, phos_weights, edge_file, forest_conf, forest_out, msgsteiner, seed)
         subprocess.call(shlex.split(fcmd), shell=False)	
 
         curr_dir = os.path.dirname(__file__)
@@ -51,9 +41,8 @@ def test_integration(msgsteiner):
             print 'Mismatching files: ', mismatch
             print 'Errors: ', errors
             assert 0, 'Not all Forest output files match'
-        else:
-            assert 1
-    except IOError as e:
+
+    except IOError:
         print 'IO error'
     finally:
         shutil.rmtree(forest_out)
